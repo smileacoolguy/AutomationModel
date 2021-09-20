34f273a874ed027e3a589bcbcc3cb592b87e0bd0
smileacoolguy <mohammed.ismail@nextory.com>
2021-09-20 12:00:50 +0530

    rough test

--- src/test/java/com/nestadmin/Rough.java
+++ src/test/java/com/nestadmin/Rough.java
@@ -4,8 +4,13 @@ import java.util.Arrays;
 import com.nestadmin.BaseTestClass;
 
 public class Rough {
-
+	public static void logg(String s) {
+		
+		BaseTestClass.log4j2.info(s);
+	}
 	public static void main(String[] args) {
+		
+
 		// TODO Auto-generated method stub
 Object[][] ob=new Object[2][2];
 ob[1][0]="ajaz";
@@ -15,8 +20,11 @@ System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
 
 
 //System.out.println(log4j2);
-BaseTestClass log=new BaseTestClass();
+BaseTestClass log=new com.nestadmin.BaseTestClass();
 BaseTestClass.log4j2.info("test");
+
+//log.info("ajaz");
+logg("a");
 	}
 
 }
